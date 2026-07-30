8fa2cf5d58f1f59a3937a25e0bcacabfca2e6952
totoya503 <angeleme503@gmail.com>
2026-07-29 21:37:45 -0600

    esto añade otro error

---
+++ database/migrations/2026_07_30_033721_ejemplo.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+   
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+
